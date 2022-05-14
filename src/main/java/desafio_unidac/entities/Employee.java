cc9485e2f17f3a1f5ece5b3a3857b4fd8305cb74
Calx1988 <cassioalexsander@gmail.com>
2022-05-14 17:55:00 -0300

    Created classes employee and recipe

---
+++ src/main/java/desafio_unidac/entities/Employee.java
@@ -0,0 +1,86 @@
+package desafio_unidac.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String cpf;
+	private List<Recipe> listRecipe = new ArrayList<>();
+	
+	public Employee() {
+	}
+
+	public Employee(Long id, String name, String cpf, List<Recipe> listRecipe) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.listRecipe = listRecipe;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public List<Recipe> getListRecipe() {
+		return listRecipe;
+	}
+
+	public void setListRecipe(List<Recipe> listRecipe) {
+		this.listRecipe = listRecipe;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		return Objects.equals(cpf, other.cpf) && Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/desafio_unidac/entities/Recipe.java
@@ -0,0 +1,66 @@
+package desafio_unidac.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Recipe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	
+	public Recipe() {
+	}
+
+	public Recipe(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Recipe other = (Recipe) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
